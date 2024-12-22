c95ce2722fc47c7f60f073f54cc7a864f5c48e34
Oliks <dlyaegr1@gmail.com>
2024-12-23 00:59:31 +0300

    Update lzw.php

--- lzw.php
+++ lzw.php
@@ -1,3 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LZW</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<style>
+        body {
+            background-color: #f8f9fa;
+        }
+        .container {
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 20px;
+            background: #ffffff;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+</style>
+<body>
+    <header>
+        <div class="container">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/Oliks9lb4/lzw.php">LZW эффективное</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Oliks9lb4/lzw2.php">LZW циферки</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <div class="container">
+    <h1>Алгоритм Лемпеля-Зива-Велча (LZW)</h1>
+    <h2>Сжать</h2>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToCompress" class="btn btn-outline-info" required>
+        <div class="mt-2">
+        <button type="submit" class="btn btn-outline-info btn-lg">Сжать</button>
+        </div>
+    </form>
+
+    <h2>Распаковать</h2>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToDecompress" class="btn btn-outline-warning" required>
+        <div class="mt-2">
+        <button type="submit" class="btn btn-outline-warning btn-lg">Распаковать</button>
+        </div>
+    </form>
+    </div>
+</body>
+</html>
+
 <?php
 
 function lzw_encode($input) {
@@ -94,57 +149,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LZW</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<style>
-        body {
-            background-color: #f8f9fa;
-        }
-        .container {
-            max-width: 600px;
-            margin: 50px auto;
-            padding: 20px;
-            background: #ffffff;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-        }
-</style>
-<body>
-    <header>
-        <div class="container">
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/Oliks9lb4/lzw.php">LZW эффективное</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/Oliks9lb4/lzw2.php">LZW циферки</a>
-                </li>
-            </ul>
-        </div>
-    </header>
-    <div class="container">
-    <h1>Алгоритм Лемпеля-Зива-Велча (LZW)</h1>
-    <h2>Сжать</h2>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileToCompress" class="btn btn-outline-info" required>
-        <div class="mt-2">
-        <button type="submit" class="btn btn-outline-info btn-lg">Сжать</button>
-        </div>
-    </form>
 
-    <h2>Распаковать</h2>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileToDecompress" class="btn btn-outline-warning" required>
-        <div class="mt-2">
-        <button type="submit" class="btn btn-outline-warning btn-lg">Распаковать</button>
-        </div>
-    </form>
-    </div>
-</body>
-</html>
